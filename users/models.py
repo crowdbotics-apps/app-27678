2e70e01b5459aea2785994c914c234710e45b1e3
Crowdbotics <team@crowdbotics.com>
2021-06-02 11:58:04 +0000

    Update models

--- users/models.py
+++ users/models.py
@@ -23,15 +23,3 @@ class User(AbstractUser):
         blank=True,
         max_length=255,
     )
-    emailid = models.EmailField(
-        max_length=254,
-        null=True,
-        blank=True,
-    )
-    role = models.TextField(
-        null=True,
-        blank=True,
-    )
-
-    def get_absolute_url(self):
-        return reverse("users:detail", kwargs={"username": self.username})
